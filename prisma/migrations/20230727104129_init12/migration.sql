9206d61b03fa78ed3127d2306afce2a64eaa6ae2
carloscrovara <carloscrovara@gmail.com>
2023-07-27 07:43:20 -0300

    Cambios en schema prisma

---
+++ prisma/migrations/20230727104129_init12/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updatedAt` on the `OrdersAndProducts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `OrdersAndProducts` DROP COLUMN `updatedAt`;
